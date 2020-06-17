619cecd5b48e1d9541cb57549ac90ace1c885243
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-06-17 09:54:23 +0530

    first commit

---
+++ src/firebase/FireBaseConfig.js
@@ -0,0 +1,12 @@
+var FirebaseConfig = {
+    apiKey: "AIzaSyBNKxthjbxEURIoFPVR0ZuH2ZTPAe3B3qw",
+    authDomain: "mygitapp-ea68e.firebaseapp.com",
+    databaseURL: "https://mygitapp-ea68e.firebaseio.com",
+    projectId: "mygitapp-ea68e",
+    storageBucket: "mygitapp-ea68e.appspot.com",
+    messagingSenderId: "1066677873693",
+    appId: "1:1066677873693:web:6bf464db94cd5ab7a46af4",
+    measurementId: "G-R6LF6ZY9WF"
+  };
+
+  export default FirebaseConfig;
---
+++ src/pages/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            <h1>Sign IN </h1>
+        </div>
+    );
+};
+
+export default SignIn;
