f584be553629b025dcd38a3bc5e254c43925cd5c
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-06-18 08:12:11 +0530

    Updates

---
+++ precache-manifest.6a4eec11263a2eef47df41a0265f58fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84921d53bc4fa1cfbef22b0ed8a5eba",
+    "url": "/github-search-app/index.html"
+  },
+  {
+    "revision": "eb74ad47a61d8f435fae",
+    "url": "/github-search-app/static/css/2.79dd3d24.chunk.css"
+  },
+  {
+    "revision": "de0a668c798f92c1fd10",
+    "url": "/github-search-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb74ad47a61d8f435fae",
+    "url": "/github-search-app/static/js/2.cf40f0b9.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/github-search-app/static/js/2.cf40f0b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0a668c798f92c1fd10",
+    "url": "/github-search-app/static/js/main.9fdfe3e7.chunk.js"
+  },
+  {
+    "revision": "72d58a42840c6e045694",
+    "url": "/github-search-app/static/js/runtime-main.a4aee3c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-search-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
